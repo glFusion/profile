7467d0d6d26457b1946dc66d69de77d66ebb43c2
Lee Garner <lee@leegarner.com>
2016-06-02 14:41:11 -0700

    Add notification at login to members with no email address

--- language/swedish_utf-8.php
+++ language/swedish_utf-8.php
@@ -125,6 +125,7 @@ $LANG_PROFILE = array(
 'name_format0'  => 'First Last',
 'name_format1'  => 'Last, First',
 'list_footer'   => '<span class="profile_private">Highlighted</span> entries indicate members who have not elected to be listed in the member listing.',
+'missing_email' => 'Your email address is not included in your account profile. This may happen if you log in using the Facebook link. Please visit <a href="' . $_CONF['site_url'] . '/usersettings.php?mode=edit">your account settings</a> to update your email address so you will receive notifications and subscriptions.',
 
 // begin membership fields
 'del_selected' => 'Delete Selected',
