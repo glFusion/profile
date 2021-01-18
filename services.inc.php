dfe7ca65f21125361aaf82587caade73edcf6055
Lee Garner <lee@leegarner.com>
2021-01-18 11:54:00 -0800

    Deprecate unused API functions

--- services.inc.php
+++ services.inc.php
@@ -3,7 +3,7 @@
  * Web service functions for the Profile plugin.
  *
  * @author      Lee Garner <lee@leegarner.com>
- * @copyright   Copyright (c) 2011-2020 Lee Garner <lee@leegarner.com>
+ * @copyright   Copyright (c) 2011-2021 Lee Garner <lee@leegarner.com>
  * @package     profile
  * @version     1.2.0
  * @license     http://opensource.org/licenses/gpl-2.0.php
@@ -34,16 +34,22 @@ function service_setvalue_profile($args, &$output, &$svc_msg)
 {
     global $_TABLES;
 
-    if (!isset($args['uid'])) return PLG_RET_ERROR;
-    else $uid = (int)$args['uid'];
+    if (
+        !isset($args['uid']) ||
+        $args['uid'] < 2 ||
+        !isset($args['field']) ||
+        empty($args['field'])
+    ) {
+        return PLG_RET_ERROR;
+    }
+    $uid = (int)$args['uid'];
+    $field = DB_escapeString($args['field']);
 
-    if (!isset($args['field'])) return PLG_RET_ERROR;
-    else $field = DB_escapeString($args['field']);
-
-    if ($uid < 2 || empty($field)) return PLG_RET_ERROR;
-
-    if (!isset($args['value'])) $value='';
-    else $value = DB_escapeString($args['value']);
+    if (!isset($args['value'])) {
+        $value='';
+    } else {
+        $value = DB_escapeString($args['value']);
+    }
 
     $where = "name='$field'";
     if (isset($args['plugin)'])) {
@@ -51,15 +57,23 @@ function service_setvalue_profile($args, &$output, &$svc_msg)
     }
 
     $type = DB_getItem($_TABLES['profile_def'], 'type', $where);
-    if (!$type) return PLG_RET_ERROR;
+    if (!$type) {
+        return PLG_RET_ERROR;
+    }
 
     // Verify that the column exists
     $res = DB_query("SHOW COLUMNS FROM {$_TABLES['profile_data']}
             WHERE Field = '{$field}'");
-    if (DB_numRows($res) != 1) return PLG_RET_ERROR;
+    if (DB_numRows($res) != 1) {
+        return PLG_RET_ERROR;
+    }
 
     // See if there is a record for this user.  Create one if not.
-    $puid = DB_getItem($_TABLES['profile_data'], 'puid', "puid = $uid");
+    $puid = DB_getItem(
+        $_TABLES['profile_data'],
+        'puid',
+        "puid = $uid"
+    );
     if (empty($puid)) {
         $sql = "INSERT INTO {$_TABLES['profile_data']}
             SET puid = $uid, $field = '$value'";
@@ -69,108 +83,11 @@ function service_setvalue_profile($args, &$output, &$svc_msg)
             WHERE puid = $uid";
     }
     DB_query($sql, 1);
-    if (!DB_error())
+    if (!DB_error()) {
         return PLG_RET_SUCCESS;
-    else
-        return PLG_RET_ERROR;
-}
-
-
-
-/**
- * Set the system values for a user profile.
- * This allows another plugin to update this information.
- * Similar to service_setvalue_profile(), except that this only sets the
- * known system values and doesn't need to check that the columns exist, and
- * the update is done in a single query.
- *
- * @param   array   $args       Array of sys_expires and sys_membership
- * @param   array   $output     Pointer to output array (unused)
- * @param   array   $svc_msg    Unused
- * @return  integer             Status code
- */
-function service_setSysValues_profile($args, &$output, &$svc_msg)
-{
-    global $_TABLES, $_USER;
-
-    if (!isset($args['uid']) || $args['uid'] == 0)
-        $args['uid'] = $_USER['uid'];
-    $uid = (int)$args['uid'];
-    if ($uid < 2)
-        return false;
-
-    $updates = array();
-    foreach (array('sys_expires', 'sys_membertype') as $valname) {
-        if (isset($args[$valname])) {
-            $updates[] = "$valname = '".DB_escapeString($args[$valname])."'";
-        }
-    }
-    if (empty($updates)) {
-        return PLG_RET_PRECONDITION_FAILED;
-    }
-    $upd_sql = implode(',', $updates);
-    $sql = "INSERT INTO {$_TABLES['profile_data']} SET
-            puid = $uid,
-            $upd_sql
-            ON DUPLICATE KEY UPDATE $upd_sql";
-    //echo $sql;die;
-    DB_query($sql, 1);
-    if (DB_error()) {
-        return PLG_RET_ERROR;
     } else {
-        return PLG_RET_OK;
+        return PLG_RET_ERROR;
     }
-}
-
-
-/**
- * Get the child accounts of the supplied user ID.
- *
- * @param   array   $args       Optional 'uid' element
- * @param   array   $output     Set to array of child IDs, or empty
- * @param   mixed   $svc_msg    Not used
- * @return  integer             Return code
- */
-function service_getChildAccounts_profile($args, &$output, &$svc_msg)
-{
-    global $_TABLES, $_USER;
-
-    $fld_name = 'sys_parent';
-
-    $uid = isset($args['uid']) ? (int)$args['uid'] : (int)$_USER['uid'];
-    if ($uid < 2) return PLG_RET_ERROR;
-
-    $output = array();
-    $sql = "SELECT puid FROM {$_TABLES['profile_data']} WHERE `$fld_name`=$uid";
-    $res = DB_query($sql, 1);
-    while ($A = DB_fetchArray($res, false)) {
-        $output[] = $A['puid'];
-    }
-
-    return PLG_RET_OK;
-}
-
-
-/**
- * Get the parent account of the supplied user ID.
- *
- * @param   array   $args       Optional 'uid' element
- * @param   mixed   $output     Output, set as parent uid or zero
- * @param   mixed   $svc_msg    Not used
- * @return  integer             Return code
- */
-function service_getParentAccount_profile($args, &$output, &$svc_msg)
-{
-    global $_TABLES, $_USER;
-
-    $fld_name = 'sys_parent';
-
-    $uid = isset($args['uid']) ? (int)$args['uid'] : (int)$_USER['uid'];
-    if ($uid < 2) return PLG_RET_ERROR;
-
-    $output = DB_getItem($_TABLES['profile_data'], $fld_name, "puid = $uid");
-    if (empty($output)) $output = 0;
-    return PLG_RET_OK;
 }
 
 
@@ -190,7 +107,9 @@ function service_getValues_profile($args, &$output, &$svc_msg)
     $output = array();
 
     $uid = isset($args['uid']) ? (int)$args['uid'] : (int)$_USER['uid'];
-    if ($uid < 2) return PLG_RET_ERROR;
+    if ($uid < 2) {
+        return PLG_RET_ERROR;
+    }
 
     // Get all the profile items. We don't consider a failure here to be
     // an error, there will just be nothing returned.
@@ -239,7 +158,9 @@ function service_renderForm_profile($args, &$output, &$svc_msg)
 
     $output = '';
 
-    if (COM_isAnonUser()) return PLG_RET_ERROR;
+    if (COM_isAnonUser()) {
+        return PLG_RET_ERROR;
+    }
     $uid = isset($args['uid']) ? $args['uid'] : $_USER['uid'];
     $form_id = isset($args['form_id']) ? $args['form_id'] : '';
     $P = Profile\Profile::getInstance($uid);
@@ -296,5 +217,3 @@ function service_validate_profile($args, &$output, &$svc_msg)
     }
     return empty($output) ? PLG_RET_OK : PLG_RET_ERROR;
 }
-
-?>
